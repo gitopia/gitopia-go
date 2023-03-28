bb8f1e39d89955494c29c89854dd620e69f0fb9c
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-28 18:46:01 -0500

    add offline signing and verification

---
+++ offline.go
@@ -0,0 +1,61 @@
+package gitopia
+
+import (
+	"github.com/gitopia/gitopia/x/offchain/types"
+	"github.com/pkg/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (c Client) SignMsg(data []byte) ([]byte, error) {
+	signerAddr := c.Address()
+	if signerAddr == nil {
+		return nil, errors.New("error getting signer address")
+	}
+
+	msg := types.NewMsgSignData(signerAddr, data)
+	signer, err := types.NewSignerFromClientContext(c.cc)
+	if err != nil {
+		return nil, errors.Wrap(err, "error while creating the signer object")
+	}
+
+	txObj, err := signer.Sign([]sdk.Msg{msg})
+	if err != nil {
+		return nil, errors.WithMessage(err, "error signing msg")
+	}
+
+	txData, err := c.cc.TxConfig.TxJSONEncoder()(txObj)
+	if err != nil {
+		return nil, errors.WithMessage(err, "error encoding tx")
+	}
+
+	return txData, nil
+}
+
+func (c Client) VerifyMsg(data []byte) ([]byte, string, error) {
+	txObj, err := c.cc.TxConfig.TxDecoder()(data)
+	if err != nil {
+		return nil, "", errors.WithMessage(err, "error decoding tx obj")
+	}
+
+	verifier := types.NewVerifier(c.cc.TxConfig.SignModeHandler())
+	err = verifier.Verify(txObj)
+	if err != nil {
+		return nil, "", errors.WithMessage(err, "error verifying msg")
+	}
+
+	if len(txObj.GetMsgs()) != 1 {
+		return nil, "", errors.New("required exactly one signed message")
+	}
+
+	msgSign, ok := txObj.GetMsgs()[0].(*types.MsgSignData)
+	if !ok {
+		return nil, "", errors.New("signed msg not found")
+	}
+
+	memoTx, ok := txObj.(sdk.TxWithMemo)
+	if !ok {
+		return nil, "", errors.New("memo not found")
+	}
+
+	return []byte(memoTx.GetMemo()), msgSign.GetSigner(), nil
+}
