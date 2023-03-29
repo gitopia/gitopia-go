8ad924fc19f68cf149dc6b44bb1e91beb24af4ba
janani anbarasan <janani.anbarasan@gmail.com>
2023-03-28 21:36:30 -0500

    register offchain messages

--- cmd.go
+++ cmd.go
@@ -15,6 +15,7 @@ import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	gtypes "github.com/gitopia/gitopia/x/gitopia/types"
 	rtypes "github.com/gitopia/gitopia/x/rewards/types"
+	otypes "github.com/gitopia/gitopia/x/offchain/types"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -38,6 +39,7 @@ func CommandInit(cmd *cobra.Command, appName string) error {
 	authtypes.RegisterInterfaces(interfaceRegistry)
 	gtypes.RegisterInterfaces(interfaceRegistry)
 	rtypes.RegisterInterfaces(interfaceRegistry)
+	otypes.RegisterInterfaces(interfaceRegistry)
 
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
